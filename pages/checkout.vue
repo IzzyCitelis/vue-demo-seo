f316c2282b5b6efa64d5587e88163ae2eef0a554
Isidoro <i.rodriguez@citelis.mx>
2023-01-18 21:53:27 -0600

    twter card

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -10,17 +10,19 @@
 useHead({
     titleTemplate:' %s - Checkout',
     meta:[
-        {name:'robots', content:"index, follow" },
-        {name: 'og:title', content: 'Card twitter' },
+        /* {name:'robots', content:"index, follow" },
         {name: 'og:description', content: 'Check us on twitter' },
-        {name: 'og:url', content: 'https://seo-izzy-izzycitelis.vercel.app/checkout' },
-        {name: 'og:type', content: 'website' },
+        {name: 'og:type', content: 'website' }, */
 
 
         {name: 'twitter:card', content: 'summary' },
-        {name: 'twitter:title', content: 'Site Title' },
-        {name: 'twitter:description', content: 'Check us on twitter' },
-        {name: 'twitter:image', content: 'http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg' },
+        {name: 'twitter:site', content: 'Site twt' },
+        {name: 'twitter:creator', content: 'izzy' },
+
+        {name: 'og:url', content: 'https://seo-izzy-izzycitelis.vercel.app/checkout' },
+        {name: 'og:title', content: 'Card twitter' },
+        {name: 'og:description', content: 'Check us on twitter' },
+        {name: 'og:image', content: 'http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg' },
         
 
 
