a3e25e9f9429aac45f593b0e9aa3139b3b7593bf
Isidoro <i.rodriguez@citelis.mx>
2023-01-18 21:46:20 -0600

    og cookies

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -13,15 +13,14 @@ useHead({
         {name:'robots', content:"index, follow" },
         {name: 'og:title', content: 'Card twitter' },
         {name: 'og:description', content: 'Check us on twitter' },
-        {name: 'og:image', content: 'https://picsum.photos/200/300' },
         {name: 'og:url', content: 'https://seo-izzy-izzycitelis.vercel.app/checkout' },
         {name: 'og:type', content: 'website' },
 
 
-        {name: 'twitter:card', content: 'summary_large_image' },
+        {name: 'twitter:card', content: 'summary' },
         {name: 'twitter:title', content: 'Site Title' },
         {name: 'twitter:description', content: 'Check us on twitter' },
-        //{name: 'twitter:image', content: 'https://picsum.photos/200/300' },
+        {name: 'twitter:image', content: 'http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg' },
         
 
 
