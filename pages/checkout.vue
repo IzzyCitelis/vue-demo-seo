f8587a32d07ae8d3cef39aa805bf6ff895ac18c3
Isidoro <i.rodriguez@citelis.mx>
2023-01-18 21:35:40 -0600

    meta data

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -11,10 +11,20 @@ useHead({
     titleTemplate:' %s - Checkout',
     meta:[
         {name:'robots', content:"index, follow" },
+        {name: 'og:title', content: 'Card twitter' },
+        {name: 'og:description', content: 'Check us on twitter' },
+        {name: 'og:image', content: 'https://picsum.photos/200/300' },
+        {name: 'og:url', content: 'https://seo-izzy-izzycitelis.vercel.app/checkout' },
+        {name: 'og:type', content: 'website' },
+
+
         {name: 'twitter:card', content: 'summary_large_image' },
         {name: 'twitter:title', content: 'Site Title' },
         {name: 'twitter:description', content: 'Check us on twitter' },
-        {name: 'twitter:image', content: 'https://picsum.photos/200/300' }
+        //{name: 'twitter:image', content: 'https://picsum.photos/200/300' },
+        
+
+
 
     ]
 });
